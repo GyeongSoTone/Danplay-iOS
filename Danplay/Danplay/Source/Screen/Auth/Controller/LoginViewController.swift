c9b032e199db3700418920ee11eb5201ea8ca7dc
devxsby <devxsby@gmail.com>
2022-05-09 19:03:59 +0900

    [Add] 기본 프로젝트 생성 및 기본 세팅 (#1)
    MVC 패턴으로 폴더링

--- Danplay/Danplay/Source/Screen/Auth/Controller/LoginViewController.swift
+++ Danplay/Danplay/Source/Screen/Auth/Controller/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Danplay
 //
 //  Created by subinyoon on 2022/05/09.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
-
---
+++ Danplay/Danplay/Source/Screen/Auth/Model/AuthModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthModel.swift
+//  Danplay
+//
+//  Created by subinyoon on 2022/05/09.
+//
+
+import Foundation
