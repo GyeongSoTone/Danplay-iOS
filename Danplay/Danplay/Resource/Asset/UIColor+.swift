322fe579b81d29047ef082b1fc4c63549d6f47c5
devxsby <devxsby@gmail.com>
2022-05-10 14:05:03 +0900

    [Add] 컬러 에셋 추가 (#1)

---
+++ Danplay/Danplay/Resource/Asset/UIColor+.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+.swift
+//  Danplay
+//
+//  Created by subinyoon on 2022/05/10.
+//
+
+import UIKit
+
+extension UIColor {
+    /// Main Color Set
+    class var backWhite: UIColor? { return UIColor(named: "backWhite") }
+    class var pointBlue: UIColor? { return UIColor(named: "pointBlue") }
+    class var pointYellow: UIColor? { return UIColor(named: "pointYellow") }
+    class var textBlack: UIColor? { return UIColor(named: "textBlack") }
+    class var textGrey: UIColor? { return UIColor(named: "textGrey") }
+}
